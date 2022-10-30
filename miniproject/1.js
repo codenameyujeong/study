aca42905ebce6de4793d09198db9432bfed8cec8
codenameyujeong <salatosa1@gmail.com>
2022-10-30 21:38:39 +0900

    up

---
+++ miniproject/1.js
@@ -0,0 +1,13 @@
+const bt = document.querySelector('.button_2');
+const bt_1 = document.querySelector('.button_2_1');
+const ct = document.querySelector('.count');
+
+let count = 0;
+
+bt_1.addEventListener('click', () => {
+    counter += 2480
+    bt_1.textContent = `총 상품금액 : ${counter}`
+})         
+
+
+                            
